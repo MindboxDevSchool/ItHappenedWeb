c529d13989165c1074cf95f508669372d21dd008
Roman Rubtsov <w-raven91@mail.ru>
2020-11-04 03:12:16 +0300

    private route component

---
+++ src/Components/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../Context/auth";
+
+function PrivateRoute({ component: Component, ...rest }) {
+  const { authToken } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authToken ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+}
+export default PrivateRoute;
