045a7f857405e8cc822ac9a9603551176b6bff69
elanskiyna <nikitosbaik@mail.ru>
2020-11-04 14:29:27 +0300

    add customization

--- src/Components/Api/Api.js
+++ src/Components/Api/Api.js
@@ -35,3 +35,12 @@ export const createTracker = (trackerBody) =>
         .delete(`trackers/${id}`, authorizedRequestConfig);
 
 
+      //   {
+      //     public string ScaleMeasurementUnit { get; set; }
+      //     public bool IsPhotoRequired { get; set; }
+      //     public bool IsScaleRequired { get; set; }
+      //     public bool IsRatingRequired { get; set; }
+      //     public bool IsGeotagRequired { get; set; }
+      //     public bool IsCommentRequired { get; set; }
+      //     public bool IsCustomizationRequired { get; set; }
+      // }
