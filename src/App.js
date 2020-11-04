c18d58e9ed451e6133c93e893a97d2a537016b47
Roman Rubtsov <w-raven91@mail.ru>
2020-11-04 03:10:42 +0300

    create auth context and replaced href to Link to prevent page refreshing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import './App.css';
-import RegistrationForm from './Components/RegistrationForm/RegistrationForm';
-import Main from './Components/Main/Main';
-import {Navbar, Nav} from 'react-bootstrap';
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from 'react-router-dom';
-import Tracker from './Components/Tracker/Tracker';
-import Events from './Components/Events/Events';
-import Login from './Components/RegistrationForm/Login';
-import {getUserCredentials} from './Components/Api/Api';
-import Logout from './Components/RegistrationForm/Logout';
+import "./App.css";
+import React, { useState } from "react"
+import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
+import Main from "./Components/Main/Main";
+import { Navbar, Nav } from "react-bootstrap";
+import { Route, Switch} from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+import Tracker from "./Components/Tracker/Tracker";
+import Events from "./Components/Events/Events";
+import Login from "./Components/RegistrationForm/Login";
+import { getUserCredentials } from "./Components/Api/Api";
+import Logout from "./Components/RegistrationForm/Logout";
+import PrivateRoute from "./Components/PrivateRoute";
+import { AuthContext } from "./Context/auth";
 
 function App() {
   console.log(getUserCredentials().name);
   console.log(getUserCredentials().token);
+
+  const existingToken = getUserCredentials().token;
+  const [authToken, setAuthToken] = useState(existingToken);
+  
+  const setToken = (data) => {
+    localStorage.setItem("token", data.token);
+    setAuthToken(data.token);
+  }
   return (
-    <BrowserRouter>
+    <AuthContext.Provider value={{ authToken, setAuthToken: setToken }}>
+      <BrowserRouter>
+
         <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="home">It happened</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/registration">Registration</Nav.Link>
-          <Nav.Link href="/login">LogIn</Nav.Link>
-          <Nav.Link href="/logout">LogOut</Nav.Link>
-          <Nav.Link href="/trackers">Trackers</Nav.Link>
-          <Nav.Link href="/statistics">Statistics</Nav.Link>
-        </Nav>
+        <Nav.Link><Link to="home">It happened</Link></Nav.Link>
+          <Nav className="mr-auto">
+            <Nav.Link>< Link to="/registration">Registration</Link></Nav.Link>
+            <Nav.Link><Link to="/login">LogIn</Link></Nav.Link>
+            <Nav.Link><Link to="/logout">LogOut</Link></Nav.Link>
+            <Nav.Link><Link to="/trackers">Trackers</Link></Nav.Link>
+            <Nav.Link><Link to="/statistics">Statistics</Link></Nav.Link>
+          </Nav>
         </Navbar>
         <Switch>
-           <Route path="/home" component={Main} />
-           <Route path="/registration" component={RegistrationForm} />
-           <Route path="/login" component={Login} />
-           <Route path="/logout" component={Logout} />
-           <Route path="/tracker/:id" component={Events} />
-           <Route path="/trackers" component={Tracker} />
-       </Switch>
-    </BrowserRouter>
+          <Route path="/home" component={Main} />
+          <Route path="/registration" component={RegistrationForm} />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/tracker/:id" component={Events} />
+          <PrivateRoute path="/trackers" component={Tracker} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
 export default App;
-
