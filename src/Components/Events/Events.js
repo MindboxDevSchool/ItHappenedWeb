eaddf1b1b8337ba26986e9e2fe141cfb7637bd79
Roman Rubtsov <w-raven91@mail.ru>
2020-11-07 19:23:48 +0300

    add error handling for trackers and events

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,95 +1,133 @@
 import { useParams } from "react-router-dom";
-import { deleteEvent, getEvent, getEvents, getTracker} from '../Api/Api';
-import { useEffect, useState } from 'react';
+import { deleteEvent, getEvent, getEvents, getTracker } from "../Api/Api";
+import { useEffect, useState } from "react";
 import EventForm from "../EventForm/EventForm";
-import { addEvent } from '../Api/Api';
-import { Table, Modal, Button } from "react-bootstrap";
-import EventRow from '../EventRow/EventRow'
+import { addEvent } from "../Api/Api";
+import { Table, Modal, Button, Alert } from "react-bootstrap";
+import EventRow from "../EventRow/EventRow";
 
 const Events = () => {
+  const authorizedRequestConfig = {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  };
 
-    const authorizedRequestConfig = {
-        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
-      };
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const { trackerId } = useParams();
+  const [events, setEvents] = useState([]);
+  const [eventId, setId] = useState("");
+  const [tracker, setTracker] = useState({});
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
 
-    const { trackerId } = useParams();
-    const [events, setEvents] = useState([]);
-    const [eventId, setId] = useState("");
-    const [tracker, setTracker] = useState({});
+  const closeErrorMessage = () => {
+    setTimeout(() => {
+      setIsError(false);
+    }, 3000);
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    const getEventsAsync = async () => {
+      getEvents(trackerId, authorizedRequestConfig)
+        .then((result) => setEvents(result.data))
+        .catch((e) => console.log(e));
+    };
 
-        const getEventsAsync = async () => {
-            getEvents(trackerId, authorizedRequestConfig)
-                .then(result => setEvents(result.data))
-                .catch(e => console.log(e))
-        }
+    const getTrackerAsync = async () => {
+      getTracker(trackerId, authorizedRequestConfig)
+        .then((result) => setTracker(result.data))
+        .catch((e) => {
+          setErrorMessage(e.response.data.ErrorMessage);
+          setIsError(true);
+        });
+    };
 
-        const getTrackerAsync = async () => {
-            getTracker(trackerId, authorizedRequestConfig)
-                .then(result => setTracker(result.data))
-                .catch(e => console.log(e))
-        }
+    getEventsAsync();
+    getTrackerAsync();
+  }, []);
 
-        getEventsAsync();
-        getTrackerAsync();
+  const onAddEvent = async (eventBody) => {
+    console.log(eventBody);
+    await addEvent(trackerId, eventBody, authorizedRequestConfig)
+      .then((result) => {
+        eventBody.id = result.data.id;
+        setEvents([...events, eventBody]);
+      })
+      .catch((e) => {
+        setErrorMessage(e.response.data.ErrorMessage);
+        setIsError(true);
+      });
+  };
 
-    }, []);
+  const onDeleteEvent = async () => {
+    await deleteEvent(eventId, authorizedRequestConfig)
+      .then((result) => {
+        setEvents(events.filter((e) => e.id != eventId));
+      })
+      .catch((e) => {
+        setErrorMessage(e.response.data.ErrorMessage);
+        setIsError(true);
+      });
+  };
 
-    const onAddEvent = async (eventBody) => {
-        console.log(eventBody)
-        await addEvent(trackerId, eventBody, authorizedRequestConfig)
-            .then(result => {
-                eventBody.id = result.data.id;
-                setEvents([...events, eventBody]);
-            })
-            .catch(error => console.log(error.response));
-    }
+  const showModal = (eventId) => {
+    setId(eventId);
+    handleShow();
+  };
 
-    const onDeleteEvent = async() => {
-        await deleteEvent(eventId, authorizedRequestConfig)
-        .then((result) => {
-            setEvents(events.filter((e) => e.id != eventId));
-          })
-          .catch((error) => console.log(error.response));
-    }
+  let i = 1;
+  return (
+    <div>
+      <br />
+      <h2>{tracker.name}</h2>
+      <Table striped hover variant="dark">
+        <tbody>
+          {events.map((event) => (
+            <EventRow
+              rowNumber={i++}
+              event={event}
+              showModal={showModal}
+              tracker={tracker}
+            />
+          ))}
+        </tbody>
+      </Table>
 
-    const showModal = (eventId) => {
-        setId(eventId);
-        handleShow();
-      };
+      <EventForm onAdd={onAddEvent} />
 
-    let i = 1;  
-    return (<div><br />
-        <h2>{tracker.name}</h2>
-        <Table striped hover variant="dark">
-            <tbody>
-                {
-                    events.map(event => <EventRow rowNumber={i++} event={event} showModal={showModal} tracker={tracker}/>)
-                }
-            </tbody>
-        </Table>
-
-        <EventForm onAdd={onAddEvent} /> 
-
-        <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} >
-            <Modal.Body>
-                Do you really want to delete a events? Data cannot be recovered
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Body>
+          Do you really want to delete a events? Data cannot be recovered
         </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
           </Button>
-                <Button variant="danger" onClick={e => { onDeleteEvent(); handleClose(); }}>Delete</Button>
-            </Modal.Footer>
-        </Modal>
-
-    </div>)
-}
-
+          <Button
+            variant="danger"
+            onClick={(e) => {
+              onDeleteEvent();
+              handleClose();
+            }}
+          >
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {isError && (
+        <Alert variant="danger">
+          {errorMessage} {closeErrorMessage()}
+        </Alert>
+      )}
+    </div>
+  );
+};
 
 export default Events;
