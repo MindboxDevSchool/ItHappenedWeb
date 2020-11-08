68cf1be1953fab22c9a35175fc4b73f7c221f68c
Roman Rubtsov <w-raven91@mail.ru>
2020-11-08 22:42:24 +0300

    add message on the main page

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 class Main extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return <h1>Main page</h1>
-    };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <br />
+        <h1>Event tracking app</h1>
+        <p>You can create, read, delete and update trackers</p>
+        <p>As well as create, read and delete events in your trackers   </p>
+      </>
+    );
+  }
 }
 
 export default Main;
