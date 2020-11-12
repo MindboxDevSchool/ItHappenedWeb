402f8cc8a6d3382c5f6fe325790f76ef3f796665
Roman Rubtsov <w-raven91@mail.ru>
2020-11-12 23:22:30 +0300

    move login and registration on material ui. Simplify code

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 import { registerUser } from "../Api/Api";
 import { Link, Redirect } from "react-router-dom";
-import { Form, Button, NavLink, Alert } from "react-bootstrap";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 import { useAuth } from "../../Context/auth";
+import {
+  useStyles,
+  PASSWORD_PATTERN,
+  LOGIN_PATTERN,
+  reqdFieldMsg,
+  invalidPwdMsg,
+} from "./Login";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { Button, LinearProgress } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import Grid from "@material-ui/core/Grid";
+import Alert from "@material-ui/lab/Alert";
 
-const PASSWORD_PATTERN = /^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,32}$/;
-const LOGIN_PATTERN = /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/;
-const reqdFieldMsg = "This is a required field";
-const invalidPwdMsg =
-  "Password must contain atleast eight characters, at least one letter and one number.";
 const invalidLoginMsg =
   "Username can contain alphanumeric characters (a-zA-Z0-9), only symbols: . - _ are allowed separated by alphanumeric. Minimum username length is 5 symbols";
 const schema = yup.object({
@@ -22,7 +32,6 @@ const schema = yup.object({
     .string()
     .matches(PASSWORD_PATTERN, invalidPwdMsg)
     .required(reqdFieldMsg),
-    //.oneOf([yup.ref("passwordConfirmation"), null], "Passwords must match"),
   passwordConfirmation: yup
     .string()
     .oneOf([yup.ref("password"), null], "Passwords must match"),
@@ -31,13 +40,12 @@ const schema = yup.object({
 const RegistrationForm = () => {
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState();
   const { setAuthToken } = useAuth();
+  const classes = useStyles();
 
-  const postRegistration = async () => {
-    await registerUser(login, password)
+  const postRegistration = async (values) => {
+    await registerUser(values.login, values.password)
       .then((result) => {
         if (result.status === 200) {
           setAuthToken(result.data.token);
@@ -57,96 +65,81 @@ const RegistrationForm = () => {
   }
 
   return (
-    <>
-      <Formik
-        validationSchema={schema}
-        validateOnChange={true}
-        onSubmit={postRegistration}
-        initialValues={{
-          login: "",
-          password: "",
-          passwordConfirmation: "",
-        }}
-      >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          touched,
-          errors,
-        }) => {
-          return (
-            <>
-              <Form noValidate onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicRegistration">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type="text"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Formik
+          validationSchema={schema}
+          onSubmit={postRegistration}
+          initialValues={{
+            login: "",
+            password: "",
+            passwordConfirmation: "",
+          }}
+        >
+          {({ isSubmitting, submitForm }) => {
+            return (
+              <>
+                <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <Form className={classes.form}>
+                  <Field
+                    component={TextField}
                     name="login"
-                    placeholder="Enter user name"
-                    value={values.login}
-                    onBlur={handleBlur}
-                    onChange={e => {
-                      handleChange(e);
-                      setLogin(values.login)}}
-                    onSubmit={setLogin(values.login)}
-                    isInvalid={errors.login}
+                    type="text"
+                    label="Login"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
                   />
-                  {touched.login && (
-                    <Form.Control.Feedback type="invalid">
-                      {errors.login}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
+                  <br />
+                  <Field
+                    component={TextField}
                     type="password"
+                    label="Password"
                     name="password"
-                    placeholder="Please enter a strong password"
-                    value={values.password}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    onSubmit={setPassword(values.password)}
-                    isInvalid={errors.password}
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
                   />
-                  {touched.password && (
-                    <Form.Control.Feedback type="invalid">
-                      {errors.password}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
-                <Form.Group controlId="formPasswordConfirmation">
-                  <Form.Label>Confirm password</Form.Label>
-                  <Form.Control
+                  <Field
+                    component={TextField}
                     type="password"
+                    label="Confirm password"
                     name="passwordConfirmation"
                     placeholder="Enter password again"
-                    value={values.passwordConfirmation}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    isInvalid={errors.passwordConfirmation}
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
                   />
-                  {touched.passwordConfirmation && (
-                    <Form.Control.Feedback type="invalid">
-                      {errors.passwordConfirmation}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Register
-                </Button>
-              </Form>
-              <NavLink>
-                <Link to="/login">Already have an account?</Link>
-              </NavLink>
-              {isError && <Alert variant="warning">{errorMessage}</Alert>}
-            </>
-          );
-        }}
-      </Formik>
-    </>
+                  {isSubmitting && <LinearProgress />}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={isSubmitting}
+                    onClick={submitForm}
+                    fullWidth
+                    className={classes.submit}
+                  >
+                    Register
+                  </Button>
+                </Form>
+
+                <Grid item>
+                  <Link to="/login">"Already have an account? Sign In"</Link>
+                </Grid>
+
+                {isError && <Alert severity="warning">{errorMessage}</Alert>}
+              </>
+            );
+          }}
+        </Formik>
+      </div>
+    </Container>
   );
 };
 
